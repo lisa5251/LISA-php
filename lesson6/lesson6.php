a9180d9515ce248c1b74f028f994901c7f72f212
lisa5251 <suliqlisa@gmail.com>
2025-04-01 16:05:54 +0200

    lesson6

---
+++ lesson6/lesson6.php
@@ -0,0 +1,32 @@
+<?php
+// $sports  = array('Real Madrid','Barcelona','Benfica');
+
+// array_unshift($sports 'Chelsea');
+
+// var_dump($sports);
+?>
+<?php
+// $sports  = array('Real Madrid','Barcelona','Benfica');
+
+// array_shift($sports);
+
+// var_dump($sports);
+?>
+<?php
+// $sports = ['Real Madrid','Barcelona','Benfica','Chelsea','Arsenal'];
+// $output = array_slice($sports,2);
+// $output1 = array_slice($sports,3);
+// $output2 = array_slice($sports,-2,1);
+
+// var_dump($output,$output1,$output2);
+?>
+<?php 
+// $sports = [12,24,36,48];
+// var_dump(array_sum($sports));
+?>
+<?php
+ $weather = [25,30,25,26,20,28,25];
+ $average_temp = array_sum($weather)/7;
+ echo($average_temp);
+ 
+?>
